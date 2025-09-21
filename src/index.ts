1f377325fb1961fdb6f90ca1147e013bde4200e9
Carlos Zanella <cefzanella@gmail.com>
2025-09-20 21:37:06 -0300

    feat: input questionaire

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { argsSchema } from './args-schema';
+import { questionaire } from './input-questionaire';
 
 async function main() {
   const args = await argsSchema.validate(require('args-parser')(process.argv));
   
-  console.log(args);
+  const options = await questionaire();
+  console.log(JSON.stringify(options, null, 2));
 }
 
 main().catch(e => {
---
+++ src/input-questionaire.ts
@@ -0,0 +1,78 @@
+import { input, select } from '@inquirer/prompts';
+
+type AppOptions = {
+  componentName: string;
+  outputFolder: string;
+  language: 'js' | 'ts';
+  layoutSystem: null | 'css' | 'module.css' | 'sass' | 'module.sass';
+}
+
+type QuestionaireProps = {
+  defaults?: Partial<Omit<AppOptions, 'componentName'>>;
+};
+
+export async function questionaire({ defaults = {} }: QuestionaireProps = {}): Promise<AppOptions> {
+  const componentName: AppOptions['componentName'] = await input({
+    message: 'Name of the component',
+    required: true,
+    validate: name => Boolean(name.match(/^\s*[A-Z][a-zA-Z0-9]*\s*$/))
+      ? true
+      : 'Component name must start with an uppercase letter and have letters and numbers only',
+  });
+
+  const outputFolder: AppOptions['outputFolder'] = await input({
+    message: 'Path to where the component will be generated',
+    required: true,
+    validate: f => f.trim().length > 0,
+    default: defaults.outputFolder,
+  });
+
+  const language: AppOptions['language'] = await select({
+    message: 'Which language do you use?',
+    choices: [
+      {
+        name: 'JavaScript',
+        value: 'js',
+      },
+      {
+        name: 'TypeScript',
+        value: 'ts',
+      },
+    ],
+    default: defaults.language,
+  });
+
+  const layoutSystem: AppOptions['layoutSystem'] = await select({
+    message: 'Which layout system do you use?',
+    choices: [
+      {
+        name: 'None/Tailwind',
+        value: null,
+      },
+      {
+        name: 'CSS',
+        value: 'css',
+      },
+      {
+        name: 'CSS modules',
+        value: 'module.css',
+      },
+      {
+        name: 'SASS',
+        value: 'sass',
+      },
+      {
+        name: 'SASS modules',
+        value: 'module.sass',
+      },
+    ],
+    default: defaults.layoutSystem,
+  });
+
+  return {
+    componentName,
+    outputFolder,
+    language,
+    layoutSystem,
+  };
+};
